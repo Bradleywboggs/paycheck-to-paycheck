34a46054d0086a98059a0d717f29556aea46494e
Brad Boggs <bboggs@bboggs-mbp.local>
2021-05-06 06:39:58 -0600

    create initial main function which prints some hard coded values and some computed values based on those make linting fixes

--- Bill.hs
+++ Bill.hs
@@ -3,14 +3,23 @@ module Bill where
 type Amount = Float
 type Date = Int
 type Name = String
-data PaymentStatus = Paid | Unpaid deriving (Eq, Show)
+data PaymentStatus = Paid | Unpaid deriving Eq
+
+instance Show PaymentStatus where
+  show paymentStatus = 
+    case paymentStatus of 
+      Paid -> "Paid"
+      Unpaid -> "Unpaid"
 
 data Bill = Bill 
   { paymentAmount :: Amount
   , paymentDate   :: Date
   , payeeName     :: Name
   , status        :: PaymentStatus
-  } deriving Show
+  } deriving (Eq)
+
+instance Show Bill where
+  show (Bill paymentAmount paymentDate payeeName status) = "Payee: " ++ payeeName ++ ", Amount: " ++ show paymentAmount ++ ", Status: " ++ show status
 
 getPaymentAmount :: Bill -> Amount
 getPaymentAmount (Bill amount date name status) = amount
@@ -25,13 +34,13 @@ checkPaymentStatus :: Bill -> PaymentStatus
 checkPaymentStatus (Bill amount date name status) = status
 
 getUnpaidBills :: [Bill] -> [Bill]
-getUnpaidBills bills = filter (\x -> Unpaid == (checkPaymentStatus x)) bills
+getUnpaidBills = filter (\x -> Unpaid == checkPaymentStatus x)
 
 getUnpaidTotal :: [Bill] -> Amount
-getUnpaidTotal bills = sum $ fmap getPaymentAmount $ getUnpaidBills bills
+getUnpaidTotal bills = sum $ getPaymentAmount <$> getUnpaidBills bills
 
 payBill :: Bill -> Bill
-payBill (Bill amount date name status) = (Bill amount date name Paid)
+payBill (Bill amount date name status) = Bill amount date name Paid
 
 updateBills :: Bill -> [Bill] -> [Bill]
-updateBills updatedBill bills =  updatedBill  : (filter (\x -> (getPayeeName updatedBill) /= (getPayeeName x)) bills)
+updateBills updatedBill bills =  updatedBill  : filter (\x -> getPayeeName updatedBill /= getPayeeName x) bills
--- CreateBill.hs
+++ CreateBill.hs
@@ -3,22 +3,22 @@ module CreateBill where
 import Bill
 import BillInputHelpers
 
-createBill :: IO (Bill)
+createBill :: IO Bill
 createBill = do
   putStrLn "Enter the Payee's name: "
   name <- getLine
   
   putStrLn "Enter the payment amount: "
   amount <- getLine
-  floatAmt <- return $ toFloat amount
+  let floatAmt = toFloat amount
 
   putStrLn "Enter the numeric payment Date (1 - 31): "
   date <- getLine
-  intDate <- return $ toInt date
+  let intDate = toInt date
   
   putStrLn "Has the bill been paid this month? (y/n): "
   yesOrNo <- getLine
-  paymentStatus <- return $ toPaymentStatus yesOrNo
+  let paymentStatus = toPaymentStatus yesOrNo
 
   return (Bill floatAmt intDate name paymentStatus)
 
